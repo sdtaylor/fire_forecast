d2198066c19a9d24feb9ccffd56898115cc86559
Shawn <shawntaylor@weecology.org>
2017-02-21 17:16:48 -0500

    put in latitude/longitude of all cells

--- simple_fire_model.R
+++ simple_fire_model.R
@@ -1,12 +1,17 @@
 library(tidyverse)
-library(magrittr)
 library(lubridate)
-library(rgeos)
-library(sp)
 library(raster)
 
 
 south_american_extent=extent(276, 328, -38, 15)
+
+#Get the latitude and longitude of all cells
+reference_raster = crop(raster('./data/precip_rasters/precip-2001.tif'), south_american_extent)
+lat_lons = as.data.frame(rasterToPoints(reference_raster))
+colnames(lat_lons) = c('lon','lat','precip')
+lat_lons$cell_id = 1:nrow(lat_lons)
+lat_lons = dplyr::select(lat_lons, -precip)
+#############################################################################
 
 all_data=data.frame()
 
@@ -21,10 +26,12 @@ for(this_year in 2001:2015){
     
     combined = as.data.frame(raster::stack(fire_raster, precip_raster))
     colnames(combined) = c('num_fires', 'precip')
+    combined$cell_id = 1:nrow(combined)
     
     combined = combined %>%
       mutate(year=this_year, month = this_month) %>%
-      filter(!is.na(num_fires))
+      filter(!is.na(num_fires)) %>%
+      left_join(lat_lons, by='cell_id')
     
     all_data = all_data %>%
       bind_rows(combined)
@@ -32,7 +39,9 @@ for(this_year in 2001:2015){
   
 }
 
+###############################################################################
 
+model = glm(num_fires ~ precip + month + lon + lat, family='poisson', data=all_data)
 
 
 
